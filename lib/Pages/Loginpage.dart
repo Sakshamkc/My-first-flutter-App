30e2b61e910ba153a7f6e3742797f473bae21b82
Sakshamkc <Sakshamkc10@gmail.com>
2021-05-28 12:12:47 +0545

    first commit

---
+++ lib/Pages/Loginpage.dart
@@ -0,0 +1,147 @@
+
+import 'package:firstproject/Pages/Registration.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool rememberme = false;
+  bool showPassword = false;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+          child: Scaffold(
+            backgroundColor: Colors.grey.shade300,
+        appBar: AppBar(
+         title: Text("Hamro App",style: TextStyle(fontSize: 24,color: Colors.white),
+         ),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Form(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                children : [
+                  Text("Welcome ,",style: TextStyle(fontSize: 24),),
+                  Text("Sign in to continue",style: TextStyle(fontSize: 24),),
+
+              SizedBox(
+                height: 20,
+              ),
+
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Email',
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.all(5),
+                      ),
+                  ),
+                   SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      suffixIcon: IconButton(
+                        
+                        onPressed: (
+                        ) {
+                          if (showPassword == false) {
+                            setState(() {
+                              showPassword = true;
+                            });
+                        }
+                        else{
+                          setState(() {
+                               showPassword = false;
+                          });
+                        }
+                        }, icon: Icon(Icons.remove_red_eye),) ,
+                      border: OutlineInputBorder(),
+                      contentPadding: EdgeInsets.all(5),
+                    ),
+                    obscureText: showPassword,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Checkbox(
+                          activeColor: Colors.green,
+                          value: rememberme, onChanged:(bool value) {
+                          setState(() {
+                            rememberme = value;
+                          });
+                        }),
+                        Text("Remember Me ?",style: TextStyle(fontSize: 14),),
+                      ],
+                  ),
+                  Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text("Forgot Password?",style: TextStyle(
+                      color: Colors.green,
+                      decoration: TextDecoration.underline,
+                      fontSize: 14,
+                    ),),
+                  ],
+
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                    Row(
+                      children: [
+                        Expanded(
+                            child: MaterialButton(
+                            elevation: 10,
+                          color: Colors.green,
+                          onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text("LOGIN",style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                    ),),
+                  ),
+
+                  ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                   Row(
+                       mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => Registration()));
+                          },
+                           child: RichText(text: TextSpan(
+                                text: "Register for free? ",style: TextStyle(color: Colors.green,decoration: TextDecoration.underline ),
+                          ),
+                           
+                          ),
+                      
+                    ),
+                    ],
+                     ),
+                ],
+              ),
+            ),
+          ),
+        ),
+
+      ),
+    );
+  }
+}
---
+++ lib/Pages/Registration.dart
@@ -0,0 +1,133 @@
+import 'package:firstproject/Pages/Loginpage.dart';
+import 'package:flutter/material.dart';
+
+class Registration extends StatefulWidget {
+  @override
+  _RegistrationState createState() => _RegistrationState();
+}
+
+class _RegistrationState extends State<Registration> {
+   List <String> genders = ['Male','Female',];
+  String gender;
+  bool name =false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Registration Form")
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+                    child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                        labelText: 'Full NAME',
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.all(5),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextField(
+                  decoration: InputDecoration(
+                        labelText: 'LAST NAME',
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.all(5),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextField(
+                  decoration: InputDecoration(
+                        labelText: 'Address',
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.all(5),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                     DropdownButton <String>(
+                      isExpanded: true,
+                      value: gender,
+                      hint: Text("Select Gender"),
+                      items: genders.map((String value) {
+                        return DropdownMenuItem <String>(
+                          value: value,
+                          child: Text(value),
+                          );
+                      }).toList(),
+                    onChanged: (
+                      String value
+                    ) {
+                      setState(() {
+                        gender = value;
+                      });
+                    }),
+                    Row(
+                    children: [
+                      Checkbox(
+                          activeColor: Colors.green,
+                          value: name, onChanged:(bool value) {
+                          setState(() {
+                            name = value;
+                          });
+                        }),
+                        Text("I accept terms and condition"),
+                      ],
+                  ),
+                  SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                            child: MaterialButton(
+                            elevation: 10,
+                          color: Colors.green,
+                          onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text("Proceed",style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                    ),),
+                  ),
+
+                  ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                     Row(
+                    
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                      },
+                     child: RichText(text: TextSpan(
+                                text: "Go back ?",style: TextStyle(color: Colors.green,decoration: TextDecoration.underline ),
+                          ),
+                          ),
+                    ),
+                  ],
+
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
