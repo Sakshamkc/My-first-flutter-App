a46992e0a6da8ba8e4387a535127ecdc4a0a3344
Sakshamkc <Sakshamkc10@gmail.com>
2021-06-01 10:01:33 +0545

    carousel added

--- lib/navbar.dart/Home.dart
+++ lib/navbar.dart/Home.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -9,7 +10,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Text("This is homepage")),
+      child: Text("This is homepage"),
     );
   }
 }
