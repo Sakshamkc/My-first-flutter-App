78cf6406f9cc9359dd76de2d87f9cf1f46abf888
Sakshamkc <Sakshamkc10@gmail.com>
2021-06-06 16:44:45 +0545

    Visibility added

--- lib/navbar.dart/Account.dart
+++ lib/navbar.dart/Account.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 
@@ -8,6 +9,13 @@ class Account extends StatefulWidget {
 }
 
 class _AccountState extends State<Account> {
+  bool isVisible = false;
+  bool isVisibleee = false;
+  bool issVisiblee =false;
+  bool isVisiblee = false;
+  bool isTrue = false;
+  bool isFalse = false;
+  bool isYes = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,54 +32,123 @@ class _AccountState extends State<Account> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                   Container(
+                     child: ListTile(
+                          onTap: () {
+                            setState(() {
+                              isVisible = !isVisible;
+                            });
+                          },
+                          leading: Icon(FontAwesome.registered,color: Colors.green,),
+                          title: Text("Registration",style: TextStyle(fontWeight: FontWeight.bold),),
+                          subtitle: Text("Create a new account"),
+                          
+                        ),
+                   ),
+                      
+                  Visibility(
+                     visible: isVisible,
+                     child: Center(
+                       child: Text("You can go to a website"),
+                     ),),
+                     
                 ListTile(
-                  onTap: () {},
-                  leading: Icon(FontAwesome.registered,color: Colors.green,),
-                  title: Text("Registration",style: TextStyle(fontWeight: FontWeight.bold),),
-                  subtitle: Text("Create a new account"),
-                ),
-                
-                ListTile(
-                  onTap: () {},
+                  onTap: () {
+                    setState(() {
+                          isVisibleee = !isVisibleee;
+                      });
+                  },
                   leading: Icon(Icons.file_copy,color: Colors.green,),
                   title: Text("KYC",style: TextStyle(fontWeight: FontWeight.bold),),
                   subtitle: Text("Know your customer info"),
                 ),
+                 Visibility(
+                     visible: isVisibleee,
+                     child: Center(
+                       child: Text("You can fill up the form from our app"),
+                     ),),
                 ListTile(
-                  onTap: () {},
+                  onTap: () {
+                    setState(() {
+                                          issVisiblee = !issVisiblee;
+                                        });
+                  },
                   leading: Icon(FontAwesome.file,color: Colors.green,),
                   title: Text("Privacy Policy",style: TextStyle(fontWeight: FontWeight.bold),),
                   subtitle: Text("Know about privacy policy"),
                 ),
-                
+                Visibility(
+                     visible: issVisiblee,
+                     child: Center(
+                       child: Text("Help to know about the privacy of our App"),
+                     ),),
                 ListTile(
-                  onTap: () {},
+                  onTap: () {
+                    setState(() {
+                                          isVisiblee = !isVisiblee;
+                                        });
+                  },
+                
                   leading: Icon(Icons.login,color: Colors.green,),
                   title: Text("Login FAQs",style: TextStyle(fontWeight: FontWeight.bold),),
                   subtitle: Text("FAQs regarding login"),
                 ),
-                
+                Visibility(
+                     visible: isVisiblee,
+                     child: Center(
+                       child: Text("Helps to login regarding FAQ"),
+                     ),),
                 ListTile(
-                  onTap: () {},
+                  onTap: () {
+                    setState(() {
+                                          isTrue = !isTrue;
+                                        });
+                  },
+                  
                   leading: Icon(Icons.payment,color: Colors.green,),
                   title: Text("Payment info",style: TextStyle(fontWeight: FontWeight.bold),),
                   subtitle: Text("Information regarding payments"),
                 ),
-                
+                Visibility(
+                     visible: isTrue,
+                     child: Center(
+                       child: Text("All the information of payment is described here"),
+                     ),),
                 ListTile(
-                  onTap: () {},
+                  onTap: () {
+                    setState(() {
+                                          isFalse = !isFalse;
+                                        });
+                  },
+                  
                   leading: Icon(Icons.security,color: Colors.green,),
                   title: Text("Security Tips",style: TextStyle(fontWeight: FontWeight.bold),),
                   subtitle: Text("Tips to keep your account secure"),
                 ),
-                
+                Visibility(
+                     visible: isFalse,
+                     child: Center(
+                       child: Text("Youc can know all the security tips in this section"),
+                     ),),
                 ListTile(
-                  onTap: () {},
+                  onTap: () {
+                    setState(() {
+                                          isYes = !isYes;
+                                        });
+                  },
                   leading: Icon(Icons.contact_phone,color: Colors.green,),
                   title: Text("Contact us",style: TextStyle(fontWeight: FontWeight.bold),),
                   subtitle: Text("Have any queries? Contact us"),
                 ),
-                
+                Visibility(
+                     visible: isYes,
+                     child: Center(
+                       child: Text("The queries can be understanble if u contact here"),
+                     ),),
+
+                     SizedBox(
+                       height: 10,
+                     ),
                 Padding(
                   padding: const EdgeInsets.fromLTRB(10, 15, 20, 10),
                   child: Center(
